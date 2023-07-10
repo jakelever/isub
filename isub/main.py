ca5d864951ed010b83147af5f6a32df70483f4cc
Jake Lever <jake.lever@gmail.com>
2023-07-10 22:28:00 +0100

    Added --verbose, --print and --nosubmit functions and write permission checks and other checks

--- isub/main.py
+++ isub/main.py
@@ -9,7 +9,20 @@ import tempfile
 import time
 from subprocess import run
 import inspect
+import stat
 import isub
+
+def assertWritePermissions(directory):
+	assert os.path.isdir(directory), f"{directory} is not a directory"
+	st = os.stat(directory)
+	owner_write_permission = bool(st.st_mode & stat.S_IWUSR)
+	group_write_permission = bool(st.st_mode & stat.S_IWGRP)
+	others_write_permission = bool(st.st_mode & stat.S_IWOTH)
+
+	write_permission_warning = f"{directory} does not have the correct write permissions. The directory must be writeable by all users for correct functionality. Consider using 'chmod a+w {directory}' to fix this."
+	assert owner_write_permission, write_permission_warning
+	assert group_write_permission, write_permission_warning
+	assert others_write_permission, write_permission_warning
 
 def main():
 	default_image = 'jakelever/ml_gpu:latest'
@@ -19,7 +32,9 @@ def main():
 									formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('--script',required=False,type=str,help='Bash script to run')
 	parser.add_argument('--command',required=False,type=str,help='Command to run')
-	parser.add_argument('--printonly',action='store_true',help='Print out the job file to stdout (useful for debug) and do not submit')
+	parser.add_argument('--print',action='store_true',help='Print out the job file to stdout (useful for debug)')
+	parser.add_argument('--verbose',action='store_true',help='Dump out additional information')
+	parser.add_argument('--nosubmit',action='store_true',help='Do not submit the job')
 	parser.add_argument('--name',required=False,type=str,help='Name to give the job')
 	parser.add_argument('--logfile',required=False,type=str,help='Output name of log file (where stdout/stderr will be piped). If not provided, one will be creating using the job id')
 	parser.add_argument('--image',required=False,type=str,default=default_image,help='Docker image to use')
@@ -31,13 +46,26 @@ def main():
 	assert args.script or args.command, "Must provide --script with a Bash script or --command with a shell command"
 
 	username = os.environ['USER']
+	current_working_directory = os.getcwd()
 
 	volume_directory = f'/home/{username}/{username}vol1claim'
 	assert os.path.isdir(volume_directory), f"Could not find expected directory: {volume_directory}. Unable to launch job"
+
+	assert volume_directory in current_working_directory or os.path.realpath(volume_directory) in current_working_directory, f"isub must be run in directory or subdirectory of your volume: {volume_directory}"
+	current_working_directory = current_working_directory.replace(os.path.realpath(volume_directory), volume_directory)
+	assertWritePermissions(current_working_directory)
 	
 	base_dir = os.path.dirname(inspect.getfile(isub))
 	base_dir = base_dir.replace(os.path.realpath(volume_directory), volume_directory)
 	template_filename = f'{base_dir}/template.yml'
+
+	if args.verbose:
+		print(f"username={username}")
+		print(f"volume_directory={volume_directory}")
+		print(f"os.path.realpath(volume_directory)={os.path.realpath(volume_directory)}")
+		print(f"current_working_directory={current_working_directory}")
+		print(f"base_dir={base_dir}")
+		print(f"template_filename={template_filename}")
 	
 	assert os.path.isfile(template_filename), "Unable to find the template file"
 
@@ -63,12 +91,6 @@ def main():
 		gpu_count = 'nvidia.com/gpu: 1'
 		gpu_type = f'node-role.ida/gpu{args.gpu}: "true"'
 
-	pwd = os.environ['PWD']
-
-	#permissions_for_cwd = oct(os.stat(os.getcwd()).st_mode)
-	#print(permissions_for_cwd)
-	print("WARNING: Not checking for permissions. Will be implemented!")
-
 	now = datetime.now()
 	job_date = now.strftime("%Y-%m-%d-%H-%M-%S")
 	rand_id = "".join(random.choices(string.ascii_lowercase,k=4))
@@ -91,13 +113,19 @@ def main():
 	else:
 		logfile = f"log.{job_id}.out"
 
+	logfile = os.path.join(current_working_directory,logfile)
+	logdir = os.path.dirname(logfile)
+	assertWritePermissions(logdir)
+
+	print(f"Saving output of stdout/stderr to {logfile}")
+
 	with open(template_filename) as f:
 		template = f.read()
 
 	template = template.replace('<USERNAME>',username)
 	template = template.replace('<JOB_NAME>',job_name)
 	template = template.replace('<JOB_ID>',job_id)
-	template = template.replace('<WORKING_DIR>',pwd)
+	template = template.replace('<WORKING_DIR>',current_working_directory)
 	template = template.replace('<RUN_SCRIPT>',run_script)
 	template = template.replace('<RUN_ARGS>',run_args)
 	template = template.replace('<LOGFILE>',logfile)
@@ -106,16 +134,16 @@ def main():
 	template = template.replace('<GPU_COUNT>',gpu_count)
 	template = template.replace('<GPU_TYPE>',gpu_type)
 
-	if args.printonly:
+	if args.print:
 		print(template)
-		return
 
-	with tempfile.NamedTemporaryFile() as tf:
-		with open(tf.name,'w') as f:
-			f.write(template)
+	if not args.nosubmit:
+		with tempfile.NamedTemporaryFile() as tf:
+			with open(tf.name,'w') as f:
+				f.write(template)
 
-		p = run(['oc','create','-f',tf.name])
-		assert p.returncode == 0, f"oc return code = {p.returncode}"
+			p = run(['oc','create','-f',tf.name])
+			assert p.returncode == 0, f"oc return code = {p.returncode}"
 
 if __name__ == '__main__':
 	main()
